17c7913a9192c9648a9a21f59918359aa74f61cd
hansa31 <hansaalahakoon@gmail.com>
2021-05-04 20:10:54 +0530

    added globals

--- NodeJS/app.js
+++ NodeJS/app.js
@@ -1,1 +1,11 @@
+console.log("Hello World!");
 
+const amount = 12;
+
+if(amount < 10){
+    console.log("small number");
+}else{
+    console.log("large number");
+}
+
+console.log("End of the programe...");
---
+++ NodeJS/globals.js
@@ -0,0 +1,9 @@
+//GLOBALS
+
+console.log(__dirname);
+console.log(__filename);
+
+//print helloworld after intervals
+setInterval(() => {
+    console.log("Hello World")
+},1000)
