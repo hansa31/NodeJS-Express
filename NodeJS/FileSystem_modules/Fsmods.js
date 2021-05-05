d170a0b7b2f57b98bab31e66a7dcc4f7cf815901
hansa31 <hansaalahakoon@gmail.com>
2021-05-05 20:20:00 +0530

    FSmodule - sync finished

---
+++ NodeJS/FileSystem_modules/Fsmods.js
@@ -0,0 +1,15 @@
+//Synchronuos
+const { readFileSync, writeFileSync } = require('fs')
+
+const first = readFileSync('./conent/test1.txt','utf8')
+const second = readFileSync('./conent/test2.txt')
+
+console.log(first + '\n' + second);
+
+//to create a file
+writeFileSync('./conent/result.txt',`Here is the result : ${first} \n ${second}`)
+
+//to append
+writeFileSync(
+    './conent/result.txt',`Here is the result : ${first} \n ${second}`,
+    {flag:'a'})
--- NodeJS/path_modules/path.js
+++ NodeJS/path_modules/path.js
@@ -1,1 +1,13 @@
 const path = require('path')
+
+console.log(path.sep)
+
+//to join
+const filePath = path.join('/content','subfolder','test.txt')
+console.log(filePath);
+
+const base = path.basename(filePath)
+console.log(base);
+
+const absolute = path.resolve(__dirname,'content/subfolder/test.txt')
+console.log(absolute);
