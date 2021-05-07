20eea97b3380fa8b517d8b1d0521c2f5c851ec04
hansa31 <hansaalahakoon@gmail.com>
2021-05-07 19:23:06 +0530

    added the gitignore file for node_modules

---
+++ NodeJS/npmmodules/intronpm.js
@@ -0,0 +1,14 @@
+//npm
+//local dependency --> npm -i <packagename>
+//global dependency --> npm install -g <packagename>
+
+//package.json --> stores important info about project/package
+//npm init **** npm init -y
+
+//using the npm packages
+const _ = require('lodash')
+
+const item = [1,[2,[3,[4]]]]
+const newItems = _.flattenDeep(item)
+
+console.log(newItems);
